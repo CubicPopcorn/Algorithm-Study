53e328ef19e4c6d1747f0b289efca7f3202ce59f
CubicPopcorn <r4nddesc7@gmail.com>
2026-04-09 21:58:37 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 76 ms, Memory: 12392 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.kt
@@ -0,0 +1,21 @@
+import kotlin.math.min
+
+fun main() {
+    val n = readln().toInt()
+    var two = 0
+    var five = 0
+    for (i in 1..n) {
+        var remaining = i
+        while (remaining % 2 == 0 || remaining % 5 == 0) {
+            if (remaining % 2 == 0) {
+                two++
+                remaining /= 2
+            }
+            if (remaining % 5 == 0) {
+                five++
+                remaining /= 5
+            }
+        }
+    }
+    println(min(two, five))
+}

9651c917ce95518852b8995e232a03580cfb6d0e
CubicPopcorn <r4nddesc7@gmail.com>
2026-04-09 17:21:59 +0900

    [Bronze II] Title: Hashing, Time: 92 ms, Memory: 13480 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.kt
+++ 백준/Bronze/15829. Hashing/Hashing.kt
@@ -3,6 +3,6 @@ fun main() {
     val hash = readln().map { (it.code - 'a'.code + 1).toBigInteger() }
         .foldIndexed(0.toBigInteger()) { index, acc, value ->
             acc + value * 31.toBigInteger().modPow(index.toBigInteger(), 1234567891.toBigInteger())
-        } % 123456789.toBigInteger()
+        } % 1234567891.toBigInteger()
     println(hash)
 }

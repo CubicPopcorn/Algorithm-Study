5b60e7ebbc71d5d92c5be954757ad9a18d7a459c
CubicPopcorn <r4nddesc7@gmail.com>
2026-04-11 19:51:39 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2316 ms, Memory: 217316 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.kt
@@ -1,10 +1,10 @@
+
 import java.io.Writer
 
 fun main() = System.`in`.bufferedReader().use { reader ->
     System.out.bufferedWriter().use { writer ->
-        val size = reader.readLine().toInt()
-        val list = List(size) { reader.readLine().toInt() }
-        writer.writeLine(list.sorted().joinToString(separator = "\n"))
+        val list = List(reader.readLine().toInt()) { reader.readLine().toInt() }
+        writer.writeLine(list.sorted().joinToString("\n"))
     }
 }
 

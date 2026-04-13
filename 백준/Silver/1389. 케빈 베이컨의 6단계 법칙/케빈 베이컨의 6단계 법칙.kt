74c2565b38dc84dd372e207a2096865b1d2698a1
CubicPopcorn <r4nddesc7@gmail.com>
2026-04-14 00:36:18 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 204 ms, Memory: 28108 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.kt
@@ -0,0 +1,47 @@
+fun main() {
+    val (userCount, relationCount) = readln().split(" ").map(String::toInt)
+    val allUsers = List(userCount) { User(it + 1) }
+    repeat(relationCount) {
+        val (a, b) = readln().split(" ").map(String::toInt)
+        val userA = allUsers[a - 1]
+        val userB = allUsers[b - 1]
+        userA.friends.add(userB)
+        userB.friends.add(userA)
+    }
+    val baconNumberMap = sortedMapOf<User, Int>()
+    allUsers.forEach { interestedUser ->
+        val depthMap = mutableMapOf<User, Int>()
+        depthMap[interestedUser] = 0
+        var users = mutableSetOf(*interestedUser.friends.toTypedArray())
+        var nextUsers = mutableSetOf<User>()
+        var depth = 1
+        while (depthMap.keys.size < userCount) {
+            for (user in users) {
+                if (user in depthMap.keys) continue
+                depthMap[user] = depth
+                nextUsers.addAll(user.friends)
+            }
+            users = nextUsers
+            nextUsers = mutableSetOf()
+            depth++
+        }
+        baconNumberMap[interestedUser] = depthMap.values.sum()
+    }
+    val result = baconNumberMap.minBy { (_, baconNumber) -> baconNumber }
+    println(result.key.id)
+}
+
+data class User(val id: Int) : Comparable<User> {
+    val friends = mutableSetOf<User>()
+
+    override fun compareTo(other: User) = compareValuesBy(this, other) { it.id }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is User) return false
+
+        return id == other.id
+    }
+
+    override fun hashCode() = id
+}

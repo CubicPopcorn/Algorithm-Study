ae7fb38735b84cd5c037a9882f293656a9c2625c
CubicPopcorn <r4nddesc7@gmail.com>
2026-04-15 02:46:30 +0900

    [Silver I] Title: IOIOI, Time: 260 ms, Memory: 24044 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.kt
+++ 백준/Silver/5525. IOIOI/IOIOI.kt
@@ -6,25 +6,25 @@ fun main() {
 
     var count = 0
     var i = 0
-    while (i <= input.lastIndex - (findLength - 1)) {
-        val match = run {
-            for (j in 0..<findLength) {
-                val correctChar = if (j % 2 == 0) 'I' else 'O'
-                if (input[i + j] != correctChar) return@run false
-            }
-            return@run true
-        }
-        if (match) {
-            count++
-            i += findLength
-            while (i + 1 in input.indices
-                && input[i] == 'O'
-                && input[i + 1] == 'I') {
-                count++
-                i += 2
-            }
-        } else {
+    mainLoop@while (i <= input.lastIndex - (findLength - 1)) {
+        if (input[i] != 'I') {
             i++
+            continue
+        }
+        for (j in 0..<findLength) {
+            val correctChar = if (j % 2 == 0) 'I' else 'O'
+            if (input[i + j] != correctChar) {
+                i += j
+                continue@mainLoop
+            }
+        }
+        count++
+        i += findLength
+        while (i + 1 in input.indices
+            && input[i] == 'O'
+            && input[i + 1] == 'I') {
+            count++
+            i += 2
         }
     }
     println(count)
